aff85a4b96f5ef7bb2d77d9a54fb3ac36182eb16
David Som <103112459+VidderSC@users.noreply.github.com>
2023-10-12 09:42:14 +0200

    Updated Names to Main Folders

--- Using_Python_to_Interact_with_the_Operating_System/Week_2_Managing_files_with_Python/writing_files.py
+++
@@ -1,8 +0,0 @@
-# Careful when opening a file using "w" as it will delete
-# it's content as soon as it opens the file.
-# If you want to append new lines to the file, you should
-# use "a" instead of "w"
-
-with open("novel.txt", "w") as file:
-    file.write("It was a dark and stormy nigth.")
-
--- Using_Python_to_Interact_with_the_Operating_System/Week_4_Managing_Data_and_Processes/01_env_vars.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Windows Powershell we can see them all listed with "dir Env:"
-
-print("COMPUTERNAME: " + os.environ.get("COMPUTERNAME", ""))
-print("OS: " + os.environ.get("OS", ""))
-print("USERNAME: " + os.environ.get("USERNAME", ""))
